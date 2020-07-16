7371e23b7e9d18e759b93de0d8a67ff3535a4843
Rodrigo Tardelli <rodrigotmg@gmail.com>
2020-07-16 17:41:16 -0300

    Returning objects from TeamController

--- src/main/java/com/example/simple/controllers/TeamController.java
+++ src/main/java/com/example/simple/controllers/TeamController.java
@@ -1,9 +1,15 @@
 package com.example.simple.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.simple.models.Team;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,27 +18,40 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeamController {
 
   @GetMapping()
-  public String getAllTeams() {
-    return "TeamController.getAllTeams - GET";
+  public List<Team> getAllTeams() {
+    Team t1 = new Team();
+    t1.setId(1);
+    t1.setName("Team One");
+
+    Team t2 = new Team();
+    t2.setId(2);
+    t2.setName("Team Two");
+
+    return Arrays.asList(t1, t2);
   }
 
   @PostMapping()
-  public String addTeam() {
-    return "TeamController.addTeam - POST";
+  public Team addTeam(@RequestBody Team team) {
+    return team;
   }
 
   @GetMapping("/{id}")
-  public String getTeam() {
-    return "TeamController.getTeam - GET";
+  public Team getTeam(@PathVariable("id") String teamId) {
+    Team t1 = new Team();
+    t1.setId(Integer.parseInt(teamId));
+    t1.setName("Team One");
+
+    return t1;
   }
 
   @PutMapping("/{id}")
-  public String updateTeam() {
-    return "TeamController.updateTeam - PUT";
+  public Team updateTeam(@PathVariable("id") String teamId, @RequestBody Team team) {
+    team.setId(Integer.parseInt(teamId));
+
+    return team;
   }
 
   @DeleteMapping("/{id}")
-  public String deleteTeam() {
-    return "TeamController.deleteTeam - DELETE";
+  public void deleteTeam(@PathVariable("id") String teamId) {
   }
 }
