3b733a2634d608736ecc6d5db7f12f9d5c54b3b5
Rodrigo Tardelli <rodrigotmg@gmail.com>
2020-07-19 09:53:54 -0300

    Changed methods and returns to use ResponseEntity

--- src/main/java/com/example/simple/controllers/TournamentController.java
+++ src/main/java/com/example/simple/controllers/TournamentController.java
@@ -1,10 +1,21 @@
 package com.example.simple.controllers;
 
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.simple.models.Tournament;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,27 +23,52 @@ import org.springframework.web.bind.annotation.RestController;
 public class TournamentController {
 
   @GetMapping
-  public String getAllTournament() {
-    return "TournamentController.getAllTournament - GET";
+  public ResponseEntity<List<Tournament>> all() {
+
+    Tournament tournament1 = new Tournament();
+    tournament1.setId(1);
+    tournament1.setName("Tournament 2020");
+    tournament1.setCreatedAt(LocalDate.parse("2020-01-01"));
+
+    Tournament tournament2 = new Tournament();
+    tournament2.setId(2);
+    tournament2.setName("Tournament 2019");
+    tournament2.setCreatedAt(LocalDate.parse("2019-01-01"));
+
+    Tournament tournament3 = new Tournament();
+    tournament3.setId(3);
+    tournament3.setName("Tournament 2018");
+    tournament3.setCreatedAt(LocalDate.parse("2018-01-01"));
+
+    List<Tournament> list = Arrays.asList(tournament1, tournament2, tournament3);
+
+    return ResponseEntity.ok(list);
   }
 
   @PostMapping
-  public String addTournament() {
-    return "TournamentController.addTournament - POST";
+  public ResponseEntity add(@RequestBody Tournament tournament) {
+    return ResponseEntity.created(URI.create("/tournaments/" + tournament.getId())).build();
   }
 
   @GetMapping("/{id}")
-  public String getTournament() {
-    return "TournamentController.getTournament - GET";
+  public ResponseEntity one(@PathVariable String tournamentId) {
+    Tournament tournament1 = new Tournament();
+    tournament1.setId(1);
+    tournament1.setName("Tournament 2020");
+    tournament1.setCreatedAt(LocalDate.parse("2020-01-01"));
+
+    return ResponseEntity.ok(tournament1);
   }
 
   @PutMapping("/{id}")
-  public String updateTournament() {
-    return "TournamentController.updateTournament - PUT";
+  public ResponseEntity update(@PathVariable String tournamentId, @RequestBody Tournament tournament) {
+    tournament.setId(Integer.parseInt(tournamentId));
+
+    return ResponseEntity.ok(tournament);
   }
 
   @DeleteMapping("/{id}")
-  public String deleteTournament() {
-    return "TournamentController.deleteTournament - DELETE";
+  public ResponseEntity delete(@PathVariable String tournamentId) {
+    return ResponseEntity.noContent().build();
   }
 }
