14479e88d11d004daca2f6ca2c36bbf8f19b0a0c
Rodrigo Tardelli <rodrigotmg@gmail.com>
2020-07-20 16:57:59 -0300

    Changed controller to respond ResponseEntity objects

--- src/main/java/com/example/simple/controllers/CompetitionController.java
+++ src/main/java/com/example/simple/controllers/CompetitionController.java
@@ -1,9 +1,18 @@
 package com.example.simple.controllers;
 
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import com.example.simple.models.Competition;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,27 +21,70 @@ import org.springframework.web.bind.annotation.RestController;
 public class CompetitionController {
 
   @GetMapping
-  public String getAllCompetitions() {
-    return "CompetitionController.getAllCompetitions - GET";
+  public ResponseEntity all() {
+
+    Competition competition1 = new Competition();
+    competition1.setId(1);
+    competition1.setName("Competition One");
+    competition1.setLocal("Local Blue");
+    competition1.setObservation("Observation for competitionOne");
+    competition1.setTournamentId(1);
+    competition1.setCreatedAt(LocalDateTime.of(2020, 07, 20, 00, 00));
+    competition1.setStartedAt(LocalDateTime.of(2020, 07, 18, 14, 00));
+    competition1.setStartedAt(LocalDateTime.of(2020, 07, 18, 20, 00));
+
+    Competition competition2 = new Competition();
+    competition2.setId(2);
+    competition2.setName("Competition Two");
+    competition2.setLocal("Local Yellow");
+    competition2.setObservation("Observation for competitionTwo");
+    competition2.setTournamentId(1);
+    competition2.setCreatedAt(LocalDateTime.of(2020, 07, 20, 00, 00));
+    competition2.setStartedAt(LocalDateTime.of(2020, 07, 18, 14, 00));
+    competition2.setStartedAt(LocalDateTime.of(2020, 07, 18, 20, 00));
+
+    Competition competition3 = new Competition();
+    competition3.setId(3);
+    competition3.setName("Competition Third");
+    competition3.setLocal("Local Blue");
+    competition3.setObservation("Observation for competitionThird");
+    competition3.setTournamentId(1);
+    competition3.setCreatedAt(LocalDateTime.of(2020, 07, 20, 00, 00));
+    competition3.setStartedAt(LocalDateTime.of(2020, 07, 18, 14, 00));
+    competition3.setStartedAt(LocalDateTime.of(2020, 07, 18, 20, 00));
+
+    List<Competition> list = Arrays.asList(competition1, competition2, competition3);
+
+    return ResponseEntity.ok(list);
   }
 
   @PostMapping
-  public String addCompetition() {
-    return "CompetitionController.addCompetition - POST";
+  public ResponseEntity add(@RequestBody Competition competition) {
+    return ResponseEntity.created(URI.create("/competitions/" + competition.getId())).build();
   }
 
   @GetMapping("/{id}")
-  public String getCompetition() {
-    return "CompetitionController.getCompetition - GET";
+  public ResponseEntity one(@PathVariable String competitionId) {
+    Competition competition1 = new Competition();
+    competition1.setId(Integer.parseInt(competitionId));
+    competition1.setName("Competition One");
+    competition1.setLocal("Local Blue");
+    competition1.setObservation("Observation for competitionOne");
+    competition1.setTournamentId(1);
+    competition1.setCreatedAt(LocalDateTime.of(2020, 07, 20, 00, 00));
+    competition1.setStartedAt(LocalDateTime.of(2020, 07, 18, 14, 00));
+    competition1.setStartedAt(LocalDateTime.of(2020, 07, 18, 20, 00));
+    return ResponseEntity.ok(competition1);
   }
 
   @PutMapping("/{id}")
-  public String updateCompetition() {
-    return "CompetitionController.updateCompetition - PUT";
+  public ResponseEntity update(@PathVariable String competitionId, @RequestBody Competition competition) {
+    competition.setId(Integer.parseInt(competitionId));
+    return ResponseEntity.ok(competition);
   }
 
   @DeleteMapping("/{id}")
-  public String deleteCompetition() {
-    return "CompetitionController.deleteCompetition - DELETE";
+  public ResponseEntity delete(@PathVariable String competitionId) {
+    return ResponseEntity.noContent().build();
   }
 }
